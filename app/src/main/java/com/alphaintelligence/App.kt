c5b251910d674396657fe2769183eff0208d37aa
Eugene_Ivanov <99675396+EvgeshaIvanov@users.noreply.github.com>
2023-11-06 16:26:38 +0300

    init commit

---
+++ app/src/main/java/com/alphaintelligence/App.kt
@@ -0,0 +1,13 @@
+package com.alphaintelligence
+
+import android.app.Application
+import com.core.ktor.ktorModule
+import org.kodein.di.DI
+import org.kodein.di.DIAware
+
+class App: Application(), DIAware {
+
+    override val di: DI by DI.lazy {
+        import(ktorModule)
+    }
+}
---
+++ core/src/main/java/com/core/ktor/Ktor.kt
@@ -0,0 +1,24 @@
+package com.core.ktor
+
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+import org.kodein.di.DI
+import org.kodein.di.bind
+import org.kodein.di.singleton
+
+val ktorModule = DI.Module(name = "ktor", init = {
+    bind<HttpClient>() with singleton {
+        HttpClient(CIO) {
+            install(ContentNegotiation) {
+                json(
+                    Json {
+                        ignoreUnknownKeys = true
+                    }
+                )
+            }
+        }
+    }
+})
